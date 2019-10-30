e3556e43baebdb9d442ae0daff7007a89f4c6427
jihyunee <girl9505@naver.com>
2019-10-30 14:44:55 +0900

    implement "echo data to server" written in C language

---
+++ echo_client.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#define BUFSIZE 100
+
+void error_handling(char * message)
+{
+  fputs(message, stderr);
+  fputc("\n",stderr);
+  exit(1);
+}
+
+int main(int argc, char**argv)
+{
+  int sock;
+  char message[BUFSIZE];
+  int str_len;
+  struct sockaddr_in serv_addr;
+
+  if(argc!=3)
+  {
+    printf("Usage: %s <IP> <PROT> \n",argv[0]);
+    exit(1);
+  }
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if(sock == -1)
+    error_handling("socket() error");
+
+  memset(&serv_addr, 0 , sizeof(serv_addr));
+  serv_addr.sin_family=AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port=htons(atoi(argv[2]));
+
+  if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+    error_handling("connect() error!");
+
+  while(1)
+  {
+    // 메시지 입력,전송
+    fputs("전송할 메시지를 입력하세요(q to quit) : ",stdout);
+    fgets(message,BUFSIZE, stdin);
+
+    if(!strcmp(message,"q\n")) break;
+    write(sock, message, strlen(message));
+
+    // 메시지 수신,출력
+    str_len=read(sock, message, BUFSIZE-1);
+    message[str_len]=0;
+    printf("서버로부터 전송된 메시지 : %s \n",message);
+
+  }
+  close(sock);
+  return 0;
+}
