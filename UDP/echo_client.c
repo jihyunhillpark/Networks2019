85e3af6b7a4a74f5250bea0a03c3a00cb9d0ca46
jihyunee <girl9505@naver.com>
2019-11-01 13:09:10 +0900

    complete echo_client.c for the client of simple UDP socket programming.

---
+++ UDP/echo_client.c
@@ -0,0 +1,79 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#define BUFSIZE 100
+#define MESSAGES 3
+
+void error_handling(char * message)
+{
+  fputs(message, stderr);
+  fputs("\n",stderr);
+  exit(1);
+}
+
+int main(int argc, char**argv)
+{
+  int sock;
+  char message[BUFSIZE];
+  int str_len, addr_size, i;
+
+  char message1[BUFSIZE] = "1234567890";
+  char message2[BUFSIZE] = "ABCDEFGHIJ";
+  char message3[BUFSIZE] = "KLMNOPQRST";
+
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in from_addr;
+
+  if(argc != 3)
+  {
+    printf("Usage : %s <IP> <port>\n", argv[0]);
+    exit(1);
+  }
+  sock = socket(PF_INET,SOCK_DGRAM, 0);
+  if(sock == -1)
+      error_handling("UDP소켓 생성 오류");
+
+
+  memset(&serv_addr, 0 , sizeof(serv_addr));
+  serv_addr.sin_family=AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_addr.sin_port=htons(atoi(argv[2]));
+
+  /*새롭게 추가된 부분*/
+  if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+     error_handling("connect() error!");
+
+  send(sock, message1, strlen(message1), 0);
+  send(sock, message2, strlen(message2), 0);
+  send(sock, message3, strlen(message3), 0);
+
+  for(i = 0 ; i < 3 ; i++)
+  {
+      addr_size = sizeof(from_addr);
+      str_len = recvfrom(sock, message, BUFSIZE, 0,
+                (struct sockadrr*)&from_addr, &addr_size);
+      message[str_len] = 0;
+      printf("서버로 보낸 메시지 : %s \n", message);
+  }
+
+  while(1)
+  {
+      fputs("전송할 메시지를 입력하세요(q to quit):",stdout);
+      fgets(message, sizeof(message),stdin);
+      /* q입력시 server에 while을 탈출하여, serverdp eof를 전송하고 프로세스를 종료한다. */
+      if(!strcmp(message,"q\n")) break;
+      send(sock, message, strlen(message), 0);
+      str_len = recv(sock, message, BUFSIZE, 0);
+      message[str_len] = 0;
+      printf("서버로부터 전송된 메시지 :%s \n", message);
+
+  }
+  send(sock, message, 0, 0);
+  close(sock);
+  return 0;
+}
---
+++ UDP/echo_server.c
@@ -0,0 +1,62 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#define MESSAGES 3
+
+void error_handling(char * message)
+{
+  fputs(message, stderr);
+  fputs("\n",stderr);
+  exit(1);
+}
+
+int main(int argc, char**argv)
+{
+  int serv_sock;
+  int str_len, num = 0;
+
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in clnt_addr;
+  int clnt_addr_size;
+
+  if(argc!=3)
+  {
+    printf("Usage: %s <port> <buffer_size>\n",argv[0]);
+    exit(1);
+  }
+  int buffer_size = atoi(argv[2]);
+  int message[buffer_size];
+  serv_sock=socket(PF_INET, SOCK_DGRAM, 0);
+  if(serv_sock == -1)
+    error_handling("UDP소켓 생성 오류\n");
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(atoi(argv[1]));
+
+  if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+    error_handling("bind() error");
+
+
+  sleep(5);
+  while(1)
+  {
+    clnt_addr_size = sizeof(clnt_addr);
+    memset(&message, 0, sizeof(message));
+    str_len = recvfrom(serv_sock, message, buffer_size,0
+              ,(struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    /*받은 데이터의 길이가 0 즉, eof일 때 종료한다.*/
+    if(str_len == 0) break;
+    printf("수신 내용 : %s \n", message);
+    printf("수신 길이 : %d \n", str_len);
+    sendto(serv_sock, message, str_len, 0 ,(struct sockaddr*)&clnt_addr, sizeof(clnt_addr)) ;
+  }
+  close(serv_sock);
+  return 0;
+}
